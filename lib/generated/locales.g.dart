c60f868185eccad27af9ef095ee8388a88d9aa06
nazmulhaquenahin <nahin.cdr@gmail.com>
2024-10-10 20:51:19 +0600

    can remember theme

---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,30 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'en_US': Locales.en_US,
+    'bn_BD': Locales.bn_BD,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const appTitle = 'appTitle';
+  static const homeViewMessage = 'homeViewMessage';
+  static const switchLanguage = 'switchLanguage';
+}
+
+class Locales {
+  static const en_US = {
+    'appTitle': 'Home',
+    'homeViewMessage': 'HomeView is working',
+    'switchLanguage': 'Switch to Bangla',
+  };
+  static const bn_BD = {
+    'appTitle': 'হোম',
+    'homeViewMessage': 'হোম ভিউ কাজ করছে',
+    'switchLanguage': 'ইংরেজিতে পরিবর্তন করুন',
+  };
+}
